a6292d44589e86b1423ec2fcb05d73a7c290b914
Othmane Khess <otmankhess@gmail.com>
2023-09-29 15:19:34 +0100

    first

---
+++ src/app/anime/[animeId]/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const fetchAnime = async (animeId : any) => {
+  let req = await fetch(`https://ottoex.vercel.app/api/anime/${animeId}`)
+  let res = await req.json()
+  return res
+}
+
+type PageProps = {
+  params : {
+    animeId:any;
+  }
+}
+
+async function Anime({params : {animeId}}:PageProps) {
+  const getAnime = await fetchAnime(animeId)
+
+  return (
+    <div className='text-white'>{getAnime[0].title}</div>
+  )
+}
+
+export default Anime
+
+// export async function generateStaticParams() {
+
+//   let req = await fetch(`https://ottodb.vercel.app/api/popular/1`)
+//   let res = await req.json()
+
+//   return res.map((a:any) => ({
+//     animeId: a.url.toString(),
+//   })) 
+
+// }
---
+++ src/app/animes/page.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+const getTrending = async () => {
+  let req = await fetch(`https://api.animex.live/meta/anilist/trending?page=1&perPage=28`);
+  let res = await req.json();
+
+  return res;
+};
+
+async function Animes() {
+  const data = await getTrending();
+  console.log(data);
+  return <div className="grid grid-cols-6 gap-2">
+
+
+    {data.results.map((a:any,i:number)  => (
+        <Link href={`/anime/${a.malId}`} key={i}>
+
+            <img src={a.image} />
+            <h1>
+                {a.title.romaji}
+            </h1>
+        </Link>
+   ) )}
+  </div>;
+}
+
+export default Animes;
