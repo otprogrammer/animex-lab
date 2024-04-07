af61a7c8aff4699047a34539d039702b5b331c65
Othmane Khess <otmankhess@gmail.com>
2024-04-07 17:35:57 +0000

    lol

--- src/components/container/LatestContainer.tsx
+++ src/components/container/LatestContainer.tsx
@@ -81,6 +81,7 @@ export default function LatestContainer({data,heading}) {
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5  gap-2 py-2">
         {slicedAnime.map((anime, idx) => (
           <Link
+          key={idx}
           className=" w-full   inline-grid"
           href={{
   
